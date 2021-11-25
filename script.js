f927d94a54373777be2628c428239888175b5dc1
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-11-25 12:17:17 -0300

    fix: add return

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let textToInverseElement = document.getElementById('text_to_inverse');
 
 function inverse(str = '') {
-   let strLength = str.length - 1;
+  let strLength = str.length - 1;
   let strInversed = '';
 
   for (let i = strLength; i >= 0; i--) {
     let lastCharacter = str[i];
     strInversed += lastCharacter;
   }
+
+  return strInversed;
 }
 
 textToInverseElement.oninput = () => {
