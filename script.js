ee14ff93bdc0df8ccb4614c181bbf5d8a8ca558e
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-11-25 12:15:30 -0300

    refactor: another solving solution

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 let textToInverseElement = document.getElementById('text_to_inverse');
 
 function inverse(str = '') {
-  if (str === '') return str;
+   let strLength = str.length - 1;
+  let strInversed = '';
 
-  let splitedStr = str.split(' ');
-  let reversedSplitedStr = splitedStr.map((value, index) => {
-    if (value === ' ') return value;
-
-    let valueSplited = value.split('');
-    let valueInversed = valueSplited.reverse().join('');
-
-    return valueInversed;
-  });
-  let strInversed = reversedSplitedStr.join(' ');
-
-  return strInversed;
+  for (let i = strLength; i >= 0; i--) {
+    let lastCharacter = str[i];
+    strInversed += lastCharacter;
+  }
 }
 
 textToInverseElement.oninput = () => {
